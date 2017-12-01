f6bdcdaf4f11e758c8cf87e146b134b5e7a69ed5
Yi <antenna3mt@gmail.com>
2017-11-30 22:06:00 -0500

    server talk

--- src/main/java/com/peruselab/client/GerminalCodebaseFramework.java
+++ src/main/java/com/peruselab/client/GerminalCodebaseFramework.java
@@ -3,19 +3,9 @@ package com.peruselab.client;
 import com.peruselab.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.dom.client.*;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.*;
 import gwt.material.design.client.ui.*;
 import thothbot.parallax.core.client.RenderingPanel;
 import gwt.material.design.client.constants.*;
@@ -36,6 +26,90 @@ public class GerminalCodebaseFramework implements EntryPoint {
 	 */
 	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
+	private Widget TalkPanel() {
+		FlowPanel mainPanel = new FlowPanel();
+		final MaterialTextBox nameField = new MaterialTextBox();
+		nameField.setLabel("Name");
+		nameField.setFocus(true);
+		nameField.setText("GWT User");
+		final MaterialButton sendButton = new MaterialButton("Send");
+		final MaterialModal dialog = new MaterialModal();
+		MaterialModalContent dialogContent = new MaterialModalContent();
+		MaterialModalFooter dialogFooter = new MaterialModalFooter();
+		FlowPanel dialogVPanel = new FlowPanel();
+		final Label textToServerLabel = new Label();
+		final HTML serverResponseLabel = new HTML();
+		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
+		dialogVPanel.add(textToServerLabel);
+		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
+		dialogVPanel.add(serverResponseLabel);
+		dialogContent.add(dialogVPanel);
+		final MaterialButton closeButton = new MaterialButton("Close");
+		dialogFooter.add(closeButton);
+		dialog.add(dialogContent);
+		dialog.add(dialogFooter);
+		dialog.close();
+		mainPanel.add(nameField);
+		mainPanel.add(sendButton);
+		mainPanel.add(dialog);
+		closeButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				dialog.close();
+				sendButton.setEnabled(true);
+				sendButton.setFocus(true);
+			}
+
+		});
+		class MyHandler implements ClickHandler, KeyUpHandler {
+			public void onClick(ClickEvent event) {
+				sendNameToServer();
+			}
+
+			public void onKeyUp(KeyUpEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					sendNameToServer();
+				}
+			}
+
+			private void sendNameToServer() {
+				String textToServer = nameField.getText();
+				if (!FieldVerifier.isValidName(textToServer)) {
+					MaterialToast.fireToast("Please enter at least four characters");
+					return;
+				}
+
+				// Then, we send the input to the server.
+				sendButton.setEnabled(false);
+				textToServerLabel.setText(textToServer);
+				serverResponseLabel.setText("");
+				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
+					public void onFailure(Throwable caught) {
+						// Show the RPC error message to the user
+						MaterialToast.fireToast("Remote Procedure Call - Failure");
+
+						serverResponseLabel.setHTML(SERVER_ERROR);
+						dialog.close();
+						closeButton.setFocus(true);
+					}
+
+					public void onSuccess(String result) {
+						MaterialToast.fireToast("Remote Procedure Call");
+
+						serverResponseLabel.setHTML(result);
+						dialog.open();
+						closeButton.setFocus(true);
+					}
+				});
+
+			}
+
+		}
+		MyHandler handler = new MyHandler();
+		sendButton.addClickHandler(handler);
+		nameField.addKeyUpHandler(handler);
+		return mainPanel;
+	}
+
 	/**
 	 * This is the entry point method.
 	 */
@@ -43,8 +117,8 @@ public class GerminalCodebaseFramework implements EntryPoint {
 		MaterialCard card = new MaterialCard();
 		MaterialCardContent content = new MaterialCardContent();
 		MaterialCardTitle cardTitle = new MaterialCardTitle();
-		cardTitle.setText("GWT Material Design Test");
-		content.add(new HTML("Hello GWT Material Design"));
+		cardTitle.setText("Talk to Server");
+		content.add(TalkPanel());
 		card.add(cardTitle);
 		card.add(content);
 
